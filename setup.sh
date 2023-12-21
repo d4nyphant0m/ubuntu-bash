900afddff04dca8c08ae7abf7a26440f421bc64d
Daniel <99770175+d4nyphant0m@users.noreply.github.com>
2023-12-21 17:15:43 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -14,6 +14,10 @@ sudo apt-get install git-all -y &
 wait
 
 # Installing zsh
+sudo apt-get install zsh -y &
+wait
+
+# Installing zsh
 sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)" &
 wait
 
