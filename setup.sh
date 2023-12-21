7f9569c826e2a158738361788a7db6a719887e5c
Daniel <99770175+d4nyphant0m@users.noreply.github.com>
2023-12-21 17:02:08 +0100

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Update Machine
+sudo apt-get update -y &
+sudo apt-get upgrade -y &
+wait
+
+# Installing Git
+sudo apt-get install git -y &
+wait
+
+# Installing Zip
+sudo apt-get install git-all -y &
+wait
+
+# Installing zsh
+sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)" &
+wait
+
+# Output message
+echo "okeeeletsgoooo"
